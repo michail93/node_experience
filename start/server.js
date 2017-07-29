e5a6fbdb4aa67e8edab221a90ec9788663aa3c37
michail <shirokov.mixail@gmail.com>
2017-07-30 04:39:01 +0500

    add new files

--- start/server.js
+++ start/server.js
@@ -1,6 +1,23 @@
-var user= require("./user")
+var db = require("./db");
+db.connect();
 
-var name1 = new user.User("user1");
-var name2 = new user.User("user2");
+var User= require("./user")
 
-name1.hello(name2);
+function run(){
+  // var name1 = new user.User("user1");
+  // var name2 = new user.User("user2");
+
+  var name1 = new User("user1");
+  var name2 = new User("user2");
+
+  name1.hello(name2);
+  console.log(db.getPhrase("Run successfull"))
+};
+
+if (module.parent){
+  exports.run=run;
+}else{
+  run();
+}
+
+// console.log(module)
--- start/user/index.js
+++ start/user/index.js
@@ -1,18 +1,20 @@
 // exports
 // global not used
 
-var phrases = require("./ru");
+var db = require("../db");
+db.connect();
 
 function User(name){
   this.name=name;
 }
 
 User.prototype.hello = function(who){
-  console.log(phrases.Hello +", "+who.name)
+  console.log(db.getPhrase("Hello") +", "+who.name)
 }
 
 console.log("user.js is required!");
 
-exports.User=User;
+// exports.User=User;
+module.exports= User;
 
-console.log(module);
+// console.log(module);
