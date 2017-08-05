cb29a4b141747ca09f23ad5620476d2a87f7355e
Michail <shirokov.mixail@gmail.com>
2017-08-05 16:50:30 +0500

    module utils

---
+++ top/inspect.js
@@ -0,0 +1,15 @@
+var util = require("util");
+
+var obj={
+  a:5,
+  b:6,
+  // inspect : function(){
+  //   return 123;
+  // },
+};
+
+obj.self = obj;
+
+// console.log(obj)
+
+console.log(util.inspect(obj));
