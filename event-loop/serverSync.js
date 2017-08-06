a96d8db3255a1094566f2224b08ec47084bef1c6
Michail <shirokov.mixail@gmail.com>
2017-08-07 01:15:10 +0500

    1

--- event-loop/serverSync.js
+++ event-loop/serverSync.js
@@ -1,19 +1,25 @@
 var http = require("http");
 var fs = require("fs");
+var url = require("url");
+
+
 
 http.createServer(function(req, res){
   var info;
+  var get_url;
 
   console.log(req.method, req.httpVersion, req.headers);
 
   if (req.url=="/"){
     try{
-      info=fs.readFileSync("hello.html");
+      info=fs.readFileSync("index.html");
     }catch(err){
       console.log(err);
       res.statusCode=500;
       res.end("Internal server error");
     }
+    get_url=url.parse(req.url);
+    console.log(req.method);
     res.end(info);
   }else{
     res.statusCode=404;
